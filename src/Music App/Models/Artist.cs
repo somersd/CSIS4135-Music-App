f8398cc06bf6247708a4a340f9f180e1344d5993
David Somers <somersd@go.stockton.edu>
2016-10-14 11:28:26 -0400

    Add Models

---
+++ src/Music App/Models/Artist.cs
@@ -0,0 +1,9 @@
+﻿namespace Music_App.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ src/Music App/Models/MusicDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_App.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext(DbContextOptions<MusicDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
