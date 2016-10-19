237e3d9cb9c8a127ecbca66d3666e8fb71509e10
David Somers <somersd@go.stockton.edu>
2016-10-19 12:13:54 -0400

    Final commit, moving to MusicFall2016

---
+++ src/MusicApp/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music_App.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public Artist Artist { get; set; }
+        public int ArtistID { get; set; }
+        public Genre Genre { get; set; }
+        public int GenreID { get; set; }
+
+    }
+}
